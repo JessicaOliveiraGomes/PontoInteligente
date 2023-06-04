ae8acd8d5d68bcff0963c393ecb2132a1f8bf716
JessicaOliveiraGomes <jessicadeoliveiragomes@hotmail.com>
2023-06-04 13:10:49 -0300

    add

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor( private fb: FormBuilder, private snackBar: MatSnackBar/*, private router: Router*/) { }
 
   ngOnInit(): void {
+    this.gerarForm();
+  }
+
+  gerarForm () {
+    this.form = this.fb.group ({ 
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  logar() {
+    if (this.form.invalid) {
+      this.snackBar.open (
+        "Dados inválidos", "Erro", {duration: 5000});
+        return;
+    }
+    alert(JSON.stringify(this.form.value));
   }
 
 }
