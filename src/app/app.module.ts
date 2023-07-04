b8ac20effda202478805652402cf79de2b502c0c
JessicaOliveiraGomes <jessicadeoliveiragomes@hotmail.com>
2023-07-04 18:38:22 -0300

    Update app.module.ts
    Criando o módulo e componente de cadastro de pessoa jurídica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginModule, LoginRoutingModule } from './autenticacao'; 
+import { LoginModule, LoginRoutingModule, CadastroPjModule } from './autenticacao'; 
 
 @NgModule({
   declarations: [
@@ -17,7 +16,9 @@ import { LoginModule, LoginRoutingModule } from './autenticacao';
     MatButtonModule,
     LoginModule,
     LoginRoutingModule,
+    CadastroPjModule,
 
+    
     AppRoutingModule
   ],
   providers: [],
