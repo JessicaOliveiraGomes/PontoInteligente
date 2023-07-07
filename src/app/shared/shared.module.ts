340283841c61b9a8b5bcafae732fcc76cdd1349d
JessicaOliveiraGomes <jessicadeoliveiragomes@hotmail.com>
2023-07-07 18:57:40 -0300

    Criando a mascara
    Criando a diretiva de máscara de entrada de dados

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MascaraDirective } from './directives/mascara.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MascaraDirective
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MascaraDirective
   ]
 })
 export class SharedModule { }
